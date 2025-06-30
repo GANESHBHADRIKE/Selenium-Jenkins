1493d7de52122f9d269735ea6a20c0f216255b98
Ganesh_Bhadrike <bhadrikeganesh3@gamil.com>
2025-06-30 19:20:35 +0530

    Added selenium testng project

---
+++ src/test/java/allstuff/ApplicationLaunch.java
@@ -0,0 +1,35 @@
+package allstuff;
+
+import org.openqa.selenium.chrome.ChromeDriver;
+import org.openqa.selenium.chrome.ChromeOptions;
+import org.testng.Assert;
+import org.testng.annotations.Test;
+
+
+import java.time.Duration;
+
+import org.openqa.selenium.By;
+import org.openqa.selenium.Keys;
+import org.openqa.selenium.WebDriver;
+import org.openqa.selenium.WebElement;
+
+import io.github.bonigarcia.wdm.WebDriverManager;
+
+public class ApplicationLaunch {
+	
+	@Test
+	public void Amazon1() throws InterruptedException {
+	WebDriver driver = new ChromeDriver();
+	WebDriverManager.chromedriver().setup();
+    ChromeOptions options = new ChromeOptions();
+
+    options.setAcceptInsecureCerts(true);
+
+	driver.manage().timeouts().implicitlyWait(Duration.ofSeconds(10));
+	driver.get("https://www.amazon.in/");
+	driver.manage().window().maximize();
+	driver.quit();
+	
+
+	}
+}
